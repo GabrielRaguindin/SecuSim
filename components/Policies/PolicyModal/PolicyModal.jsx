6c59939758dd16c84d8b8ad8012c25af9bca7b87
law <borabolawrence@gmail.com>
2024-09-07 01:13:40 +0800

    new: clickable label and implemented esc button for closing modal

--- components/Policies/PolicyModal/PolicyModal.jsx
+++ components/Policies/PolicyModal/PolicyModal.jsx
@@ -1,11 +1,31 @@
 'use client';
 
 import { Button, Checkbox, Label, Modal } from "flowbite-react";
+import { useEffect } from "react";
 
 export default function PolicyModal({ title, description, settings, setSettings, openModal, setOpenModal, handleSavePolicy, }) {
     const handleChange = (setting) => (e) => {
         setSettings({ ...settings, [setting]: e.target.checked });
     };
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Escape') {
+            setOpenModal(false);
+        }
+    };
+
+    useEffect(() => {
+        if (openModal) {
+            document.addEventListener('keydown', handleKeyDown);
+        } else {
+            document.removeEventListener('keydown', handleKeyDown);
+        }
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [openModal]);
+
     return (
         <Modal className='font-montserrat' show={openModal} onClose={() => setOpenModal(false)}>
             <Modal.Header className='bg-gradient-to-r from-teal-400 from-10% to-teal-700 to-90%'>{title}</Modal.Header>
@@ -15,8 +35,8 @@ export default function PolicyModal({ title, description, settings, setSettings,
                 <div className="space-y-3 ml-5">
                     {Object.keys(settings).map((key) => (
                         <div key={key} className="flex items-center gap-2">
-                            <Checkbox checked={settings[key]} onChange={handleChange(key)} />
-                            <Label>{key.split(/(?=[A-Z])/).join(' ')}</Label>
+                            <Checkbox checked={settings[key]} id={key} onChange={handleChange(key)} />
+                            <Label htmlFor={key} className="hover:cursor-pointer hover:underline duration-150">{key.split(/(?=[A-Z])/).join(' ')}</Label>
                         </div>
                     ))}
                 </div>
