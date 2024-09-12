4ac7139c67b54bc01a14b1a22956dcdb2d28ecd3
Gabriel <xbladerzero@gmail.com>
2024-09-12 22:07:22 +0800

    applied escape key event

--- components/Topologies/TopologyModal/TopologyModal.jsx
+++ components/Topologies/TopologyModal/TopologyModal.jsx
@@ -1,9 +1,27 @@
 "use client";
 
 import { Modal, Button } from "flowbite-react";
+import { useEffect } from "react";
 
-export default function TopologyModal({ title, description, visualRep, openModal, setOpenModal}) {
-    
+export default function TopologyModal({ title, description, visualRep, openModal, setOpenModal }) {
+    const handleKeyDown = (e) => {
+        if (e.key === 'Escape') {
+            setOpenModal(false);
+        }
+    };
+
+    useEffect(() => {
+        if (openModal) {
+            document.addEventListener('keydown', handleKeyDown);
+        } else {
+            document.removeEventListener('keydown', handleKeyDown);
+        }
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        }
+    }, [openModal]);
+
     return (
         <Modal className="font-montserrat" show={openModal} onClose={() => setOpenModal(false)}>
             <Modal.Header className="bg-gradient-to-r from-teal-400 from-10% to-teal-700 to-90%">{title}</Modal.Header>
@@ -16,7 +34,7 @@ export default function TopologyModal({ title, description, visualRep, openModal
                 </div>
                 <div className="flex justify-end mt-5">
                     <Button color="gray" onClick={() => setOpenModal(false)}
-                            className="shadow-md transform hover:scale-105 active:scale-100 transition duration-300">
+                        className="shadow-md transform hover:scale-105 active:scale-100 transition duration-300">
                         Got it!
                     </Button>
                 </div>
